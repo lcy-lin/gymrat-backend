acba50c86d2dde2e53819b697e3d1ff72df8897c
lcy-lin <linjoy6@gmail.com>
2023-11-24 13:46:52 +0800

    style(activities): delete swagger relatives

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// import swaggerUi from 'swagger-ui-express';
-// import yaml from 'yamljs';
-// const swaggerDocument = yaml.load('./docs/swagger.yaml');
 import express from 'express';
 import bodyParser from 'body-parser';
 const app = express();
@@ -8,7 +5,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
-// app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/healthcheck', (req, res) => {
     res.send('OK');
