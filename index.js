52f431a2c125992a5d1ef6679b26c2567bd4c176
lcy-lin <linjoy6@gmail.com>
2023-11-25 14:07:13 +0800

    fix(users): add cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import userRoutes from './routes/userRoutes.js';
 import dbMiddleware from './middleware/dbMiddleware.js';
+import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(dbMiddleware);
-
+app.use(cors());
 app.use('/api/:version/users', userRoutes);
 app.get('/healthcheck', (req, res) => {
     res.send('OK');
