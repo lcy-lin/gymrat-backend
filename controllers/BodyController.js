271bd806015441af3675b85d061d3c2ecdcc6c01
lcy-lin <linjoy6@gmail.com>
2023-12-07 10:58:41 +0800

    perf(body): body create url to fit RESTful

--- controllers/BodyController.js
+++ controllers/BodyController.js
@@ -9,11 +9,12 @@ class BodyController {
             return res.status(authRes.status).json({ error: authRes.error });
         }
         try {
-            const {user_id, height, age, sex, act_level} = req.body.data;
-            if (user_id == null || height == null || age == null || sex == null || act_level == null) {
+            const {userid} = req.params;
+            const {height, age, sex, act_level} = req.body.data;
+            if (userid == null || height == null || age == null || sex == null || act_level == null) {
                 return res.status(400).json({ error: 'Client Error Response' });
             }
-            const bodyRes = await BodyModel.insertBody(user_id, height, age, sex, act_level);
+            const bodyRes = await BodyModel.insertBody(userid, height, age, sex, act_level);
             if(bodyRes.success === false && bodyRes.code === 409){
                 return res.status(409).json({ error: bodyRes.error });
             }
--- routes/bodyRoutes.js
+++ routes/bodyRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import BodyController from '../controllers/BodyController.js';
 
-router.post('/create', BodyController.createBody);
+router.post('/:userid', BodyController.createBody);
 router.get('/:id', BodyController.getBody);
 router.put('/:id', BodyController.putBody);
 router.delete('/:id', BodyController.deleteBody);
